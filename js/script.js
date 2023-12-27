251eb195d78b767b53237f50b98a5acfe6476eab
ualikhan97 <supercilium97@gmail.com>
2023-12-27 21:41:36 +0600

    abc

--- js/script.js
+++ js/script.js
@@ -5,3 +5,13 @@ burgerElement.addEventListener("click", function () {
    document.querySelector(".header__shell").classList.toggle("active");
    document.body.classList.toggle("lock");
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+   const boxes = document.querySelector(".services__box");
+
+   boxes.addEventListener("click", function () {
+      this.after(document.querySelector(".servicess__shell"));
+
+      document.querySelector(".servicess__shell").classList.toggle("show");
+   });
+});
