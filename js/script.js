0bed9362941e808ba1c34928069ad48485995351
ualikhan97 <supercilium97@gmail.com>
2024-01-04 11:31:39 +0600

    111

--- js/script.js
+++ js/script.js
@@ -3,15 +3,18 @@ let burgerElement = document.querySelector(".burger");
 burgerElement.addEventListener("click", function () {
    this.classList.toggle("active");
    document.querySelector(".header__shell").classList.toggle("active");
+
    document.body.classList.toggle("lock");
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-   const boxes = document.querySelector(".services__box");
+   const boxes = document.querySelectorAll(".services__box");
 
-   boxes.addEventListener("click", function () {
-      this.after(document.querySelector(".servicess__shell"));
+   boxes.forEach((box) => {
+      box.addEventListener("click", function () {
+         this.after(document.querySelector(".servicess__shell"));
 
-      document.querySelector(".servicess__shell").classList.toggle("show");
+         document.querySelector(".servicess__shell").classList.toggle("show");
+      });
    });
 });
